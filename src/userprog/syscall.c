3a76a3f5d36a4493b46c26f1c58bc632ea1aa7ee
mostafa <mm9542382@gmail.com>
2025-05-19 00:36:53 +0300

    add file_close syscall and trying to optimize all the function's memory

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -15,12 +15,11 @@
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
 
-static void syscall_handler(struct intr_frame *);
-void sys_exit(int status);
-static int sys_exec(const char *cmd_line);
 struct lock fs_lock;
 
+
 void verify_esp(void *esp);
+static void syscall_handler(struct intr_frame *f);
 
 const int MAX_FILE_NAME_LENGTH = 14;
 
@@ -60,35 +59,36 @@ syscall_handler(struct intr_frame *f)
     break;
   case SYS_CREATE:
     verify_string(args[0]);
-    sys_file_create(args, f);
+    f->eax = sys_file_create((const char*)args[0], (unsigned) args[1]);
     break;
   case SYS_REMOVE:
     verify_string(args[0]);
-    sys_file_remove(args, f);
+    f->eax = sys_file_remove((const char*)args[0]);
     break;
   case SYS_OPEN:
     verify_string(args[0]);
-    sys_file_open(args, f);
+    f->eax = sys_file_open((const char*) args[0]);
     break;
   case SYS_FILESIZE:
-    sys_file_size(args, f);
+    f->eax = sys_file_size(args[0]);
     break;
   case SYS_READ:
     verify_buffer(args[1], args[2]);
-    sys_file_read(args, f);
+    f->eax = sys_file_read(args[0], (void *) args[1], (unsigned) args[2]);
     break;
   case SYS_WRITE:
     verify_buffer(args[1], args[2]);
-    sys_file_write(args, f);
+    f->eax = sys_file_write(args[0], (void *) args[1], (unsigned) args[2]);
     break;
   case SYS_SEEK:
-    sys_file_seek(args);
+    sys_file_seek(args[0], (off_t) args[1]);
     break;
   case SYS_TELL:
-    sys_file_tell(args, f);
+    f->eax = sys_file_tell(args[0]);
     break;
   case SYS_CLOSE:
-    // implement close syscall
+    // verify_buffer(args[0], 1);
+    syscall_close(args[0]);
     break;
   default:
     sys_exit(-1);
@@ -191,24 +191,15 @@ void sys_exit(int status)
   }
 
   for (int i = SYSTEM_FILES; i < MAX_FILES_PER_PROCESS; i++)
-  {
     if (current_thread->files[i] != NULL)
-    {
-      lock_acquire(&fs_lock);
-      file_close(current_thread->files[i]->file_ptr);
-      lock_release(&fs_lock);
-      free(current_thread->files[i]);
-      current_thread->files[i] = NULL;
-    }
-  }
+      syscall_close(i);
 
   printf("%s: exit(%d)\n", current_thread->name, status);
   thread_exit();
 }
 
-struct file *my_get_file(int fd, const char *caller_name)
+struct file *my_get_file(int fd)
 {
-
   // printf("my get file and fd is : %d caller is : %s\n", fd, caller_name);
   struct thread *t = thread_current();
   struct open_file *cur_file = t->files[fd];
@@ -217,18 +208,16 @@ struct file *my_get_file(int fd, const char *caller_name)
   return cur_file->file_ptr;
 }
 
-void sys_file_open(int *args, struct intr_frame *f)
+int 
+sys_file_open(const char* file_name)
 {
 
-  const char *empty_str = "", *file_name = (const char *)args[0];
+  const char *empty_str = "";
   // printf("file open is called file name is : %s\n", file_name);
   if (file_name == NULL)
     sys_exit(-1);
   else if (!strcmp(file_name, empty_str))
-  {
-    f->eax = -1;
-    return;
-  }
+    return -1;
 
   // printf("file name is empty\n file name : %s\n", file_name);
   // printf("reached here in open -> 1 file name is : %s\n", file_name);
@@ -244,115 +233,108 @@ void sys_file_open(int *args, struct intr_frame *f)
     file->file_ptr = opened_file;
     file->name = file_name;
     t->files[t->files_cnt] = file;
-    f->eax = t->files_cnt++;
+    return t->files_cnt++;
     // printf("reached here in open -> 2 file name is : %s returned fd is : %d\n", file_name, t->files_cnt - 1);
   }
   else
-  {
-    f->eax = -1;
-  }
+    return -1;
 }
 
-void sys_file_write(int *args, struct intr_frame *f)
+int
+sys_file_write(int fd, void* buffer, unsigned size)
 {
-  int fd = args[0], written_bytes = args[2];
 
   if (fd < 0 || fd >= MAX_FILES_PER_PROCESS)
-  {
-    f->eax = 0;
-    return;
-  }
+    return 0;
 
   if (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO)
   {
-    handle_sys_files(fd, (const char *)args[1], (size_t)args[2]);
-    f->eax = written_bytes;
+    handle_sys_files(fd, buffer, size);
+    return size;
     // printf("write finish\n");
-    return;
   }
 
-  const char *name = "sys write";
-  struct file *cur_file = my_get_file(fd, name);
+  struct file *cur_file = my_get_file(fd);
 
   lock_acquire(&fs_lock);
-  f->eax = file_write(cur_file, (const void *)args[1], (off_t)args[2]);
+  size = file_write(cur_file, buffer, (off_t) size);
   lock_release(&fs_lock);
+
+  return size;
 }
 
-void handle_sys_files(int fd, const char *buffer, off_t size)
+void handle_sys_files(int fd, const char *buffer, unsigned size)
 {
   if (fd == STDOUT_FILENO)
   {
     // printf("putuf is called\n");
-    putbuf(buffer, size);
+    putbuf(buffer, (off_t) size);
     // printf("putuf returned\n");
   }
   else
     sys_exit(-1);
 }
 
-void sys_file_seek(int *args)
+void sys_file_seek(int fd, unsigned new_pos)
 {
-  int fd = args[0];
   if (fd < SYSTEM_FILES)
     sys_exit(-1);
 
-  const char *name = "sys seek";
-  struct file *cur_file = my_get_file(fd, name);
+  struct file *cur_file = my_get_file(fd);
 
   lock_acquire(&fs_lock);
-  file_seek(cur_file, (off_t)args[1]);
+  file_seek(cur_file, (off_t) new_pos);
   lock_release(&fs_lock);
 }
 
-void sys_file_tell(int *args, struct intr_frame *f)
+int
+sys_file_tell(int fd)
 {
-  int fd = args[0];
   if (fd < SYSTEM_FILES)
-    sys_exit(-252);
+    sys_exit(-1);
 
-  const char *name = "sys tell";
-  struct file *cur_file = my_get_file(fd, name);
+  struct file *cur_file = my_get_file(fd);
 
   lock_acquire(&fs_lock);
   off_t off = file_tell(cur_file);
   lock_release(&fs_lock);
 
-  f->eax = off;
+  return off;
 }
 
-int sys_file_size(int *args, struct intr_frame *f)
+int sys_file_size(int fd)
 {
-  int fd = args[0];
   if (fd < SYSTEM_FILES)
     sys_exit(-1);
 
-  const char *name = "sys size";
-  struct file *cur_file = my_get_file(fd, name);
+
+  struct file *cur_file = my_get_file(fd);
 
   lock_acquire(&fs_lock);
   off_t off = file_length(cur_file);
   lock_release(&fs_lock);
 
-  f->eax = off;
+  return off;
 }
 
-void sys_file_remove(int *args, struct intr_frame *f)
+bool 
+sys_file_remove(const char* file_name)
 {
   char *empty_str = "";
-  if (args[0] == NULL || !strcmp((const char *)args[0], empty_str))
+  if (file_name == NULL || !strcmp(file_name, empty_str))
     sys_exit(-1);
 
   lock_acquire(&fs_lock);
-  bool success = filesys_remove((const char *)args[0]);
+  bool success = filesys_remove(file_name);
   lock_release(&fs_lock);
 
   if (success)
-    remove_file_from_table((const char *)args[0]);
-  f->eax = success;
+    remove_file_from_table(file_name);
+  return success;
 }
 
-void remove_file_from_table(const char *name)
+void
+remove_file_from_table(const char *name)
 {
   struct thread *t = thread_current();
   for (int i = SYSTEM_FILES; i < MAX_FILES_PER_PROCESS; ++i)
@@ -367,47 +349,33 @@ void remove_file_from_table(const char *name)
   }
 }
 
-void sys_file_create(int *args, struct intr_frame *f)
+bool sys_file_create(const char* file_name, unsigned size)
 {
   char *empty_str = "";
-  if (args[0] == NULL || !strcmp((const char *)args[0], empty_str))
+  if (file_name == NULL || !strcmp(file_name, empty_str))
     sys_exit(-1);
 
-  if (strlen((char *)args[0]) > MAX_FILE_NAME_LENGTH)
-  {
-    f->eax = 0;
-    return;
-  }
+  if (strlen(file_name) > MAX_FILE_NAME_LENGTH)
+    return 0;
 
   lock_acquire(&fs_lock);
-  bool success = filesys_create((const char *)args[0], (off_t)args[1]);
+  bool success = filesys_create(file_name, (off_t)size);
   lock_release(&fs_lock);
-  f->eax = success;
+  return success;
 }
 
-void sys_file_read(int *args, struct intr_frame *f)
+int
+sys_file_read(int fd, void* buffer, unsigned length)
 {
-  int fd = args[0];
-  const void *buffer = (const void *)args[1];
-  unsigned length = args[2];
-  if (fd == NULL || buffer == NULL || length == NULL)
-  {
-    f->eax = 0;
-    return;
-  }
-
-  if (fd < 0 || fd >= MAX_FILES_PER_PROCESS)
-  {
-    f->eax = 0;
-    return;
-  }
+  if (fd == NULL || buffer == NULL || length == NULL || fd < 0 || fd >= MAX_FILES_PER_PROCESS)
+    return 0;
 
   if (fd < SYSTEM_FILES)
     handle_read_from_system_files(fd, buffer, length);
-  const char *name = "sys read";
-  struct file *file_to_read_from = my_get_file(fd, name);
+
+  struct file *file_to_read_from = my_get_file(fd);
   int actual_read = file_read(file_to_read_from, buffer, length);
-  f->eax = actual_read;
+  return actual_read;
 }
 
 int handle_read_from_system_files(int fd, void *buffer, unsigned length)
@@ -424,4 +392,32 @@ int handle_read_from_system_files(int fd, void *buffer, unsigned length)
   else
     sys_exit(-1);
   return length;
+}
+
+void
+syscall_close(int fd)
+{
+  if(fd < SYSTEM_FILES || fd > MAX_FILES_PER_PROCESS)
+    sys_exit(-1);
+
+
+
+  struct file* f = my_get_file(fd);
+  remove_file_from_table_by_fd(fd);
+
+  lock_acquire(&fs_lock);
+  file_close(f);
+  lock_release(&fs_lock);
+
+}
+
+void
+remove_file_from_table_by_fd(int fd){
+  struct thread* t = thread_current ();
+  if(t->files[fd] == NULL)
+    sys_exit(-1);
+  
+  free(t->files[fd]);
+  t->files[fd] = NULL; 
+  t->files_cnt--;
 }
